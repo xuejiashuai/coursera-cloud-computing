95955d85a4aad4820f05866b465fe71077ac6ed3
xuejiashuai <xuejs0117@gmail.com>
2026-03-01 21:56:28 -0500

    add module 6

--- module-06/create-env.sh
+++ module-06/create-env.sh
@@ -79,10 +79,10 @@ else
     # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/rds/describe-db-instances.html
     echo "******************************************************************************"
     echo "Retrieving the RDS Endpoint Address and printing to the screen..."
-    RDS_Address=$(aws rds describe-db-instances --db-instance-identifier --query "")
+    RDS_Address=$(aws rds describe-db-instances --db-instance-identifier ${22} --query "")
     echo $RDS_Address
     echo "Retrieving the RDS Read Replica Endpoint Address and printing to the screen..."
-    RDS_RR_Address=$(aws rds describe-db-instances --db-instance-identifier  --query "")
+    RDS_RR_Address=$(aws rds describe-db-instances --db-instance-identifier ${22}-read-replica --query "")
     echo $RDS_RR_Address
 # End of main if
 fi
