895e75f0740069b80f13dba36eb4280a74074560
xuejiashuai <xuejs0117@gmail.com>
2026-03-05 13:46:29 -0500

    create module 9

--- module-09/main.tf
+++ module-09/main.tf
@@ -206,7 +206,7 @@ resource "aws_autoscaling_group" "bar" {
 
 resource "aws_autoscaling_attachment" "example" {
   # Wait for lb to be running before attaching to asg
-  depends_on  = [aws_lb.lb]
+  depends_on  = [aws_lb.lb, aws_lb_target_group.alb-lb-tg]
   autoscaling_group_name = var.asg-name
   lb_target_group_arn    = aws_lb_target_group.alb-lb-tg.arn
 }
