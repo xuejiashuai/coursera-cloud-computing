47819b9d5c1881977d4ce95b0997a711da459551
xuejiashuai <xuejs0117@gmail.com>
2026-04-19 19:09:47 +0000

    upload

---
+++ ga2ex11-log.sh
@@ -0,0 +1,57 @@
+Sun Apr 19 18:40:45 UTC 2026 - Account created for Alice
+Generating public/private ed25519 key pair.
+Your identification has been saved in /home/Alice/.ssh/id_ed25519_Alice
+Your public key has been saved in /home/Alice/.ssh/id_ed25519_Alice.pub
+The key fingerprint is:
+SHA256:+otyQQ1dqbcIzysrH8VztyJmh7EH5VVh/TYUZxOx8z8 root@ubuntu-jammy
+The key's randomart image is:
++--[ED25519 256]--+
+|       . ...  +B*|
+|      . . .  o o=|
+|       o .. . .o.|
+|      o.oo..   .=|
+|     . +Soo..  .o|
+|      .o+B.. .  .|
+|      o.*.+ .  E.|
+|    o o*.+ .    .|
+|     =+oo.       |
++----[SHA256]-----+
+Sun Apr 19 18:40:45 UTC 2026 - SSH keys created for Alice
+Sun Apr 19 18:40:46 UTC 2026 - Account created for Bob
+Generating public/private ed25519 key pair.
+Your identification has been saved in /home/Bob/.ssh/id_ed25519_Bob
+Your public key has been saved in /home/Bob/.ssh/id_ed25519_Bob.pub
+The key fingerprint is:
+SHA256:JXfd6UyR/JWor4ExKwIbPSYpwj9ETeE6UuMk9u7f5yk root@ubuntu-jammy
+The key's randomart image is:
++--[ED25519 256]--+
+|    oo.       o.o|
+|   ...       o =+|
+|.o.+o.  . o o .o+|
+|oo**o+   * o  + .|
+|..+=* . S = .  o |
+|  o+.. . o . .   |
+|   .. . .   o    |
+|  .   .E ...     |
+|   ... .+o       |
++----[SHA256]-----+
+Sun Apr 19 18:40:46 UTC 2026 - SSH keys created for Bob
+Sun Apr 19 18:40:46 UTC 2026 - Account created for Charlie
+Generating public/private ed25519 key pair.
+Your identification has been saved in /home/Charlie/.ssh/id_ed25519_Charlie
+Your public key has been saved in /home/Charlie/.ssh/id_ed25519_Charlie.pub
+The key fingerprint is:
+SHA256:ra/ZiH/8oW/AUQ/hkIygsdtqgqQuZd1QIElP6RBGqo8 root@ubuntu-jammy
+The key's randomart image is:
++--[ED25519 256]--+
+| o*o+o.. o....   |
+| oo+.+. . ooo    |
+|.  o+.     ..o   |
+|.   oo   ..   .  |
+|.. ..o. S...     |
+|o+o ...  .o      |
+|Eoo o   .. ..    |
+|o  o   . =o...   |
+|..    ..=o++o    |
++----[SHA256]-----+
+Sun Apr 19 18:40:46 UTC 2026 - SSH keys created for Charlie
